07055241a82d3726282c4c1a65d363c975adca71
resparx <ramprash.rspx@gmail.com>
2023-08-31 11:50:47 +0530

    chore: add image config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        loader: 'custom',
+        loaderFile: './custom/imageLoader.js',
+      },
+}
 
 module.exports = nextConfig
