5b1736643cf0255bd4be016bf4571e99136109d3
resparx <ramprash.rspx@gmail.com>
2023-10-03 18:46:12 +0530

    fix: content issues

--- src/constants/content/services/implementationConsultingService/erpServices/riseWithSapPrivate.ts
+++ src/constants/content/services/implementationConsultingService/erpServices/riseWithSapPrivate.ts
@@ -1,5 +1,56 @@
 const content = {
-  name: "Rise with SAP (Private) Implementation Services",
+  title: "Rise with SAP (Private) Implementation Services",
+  content: "Embrace the future with confidence. Choose Marici for Rise with SAP Implementation Services, and together, we'll embark on a journey of digital transformation that elevates your company's services to new heights. Your digital journey starts here.",
+  points: [
+    {
+      title: "Private Cloud or On-Premises Deployment",
+      content: "We offer flexibility in deployment, allowing you to choose between deploying Rise with SAP (Private) within your private cloud infrastructure or on-premises data centers. This ensures you have control over your data and infrastructure"
+    },
+    {
+      title: "Data Control and Security",
+      content: "Our services prioritize data security, governance, and compliance, ensuring that your sensitive information remains protected and aligned with regulatory requirements"
+    },
+    {
+      title: "Customization",
+      content: "We'll work closely with your organization to customize the solution to meet your unique business needs and seamlessly integrate it with your existing systems"
+    },{
+      title: "Hybrid Deployment",
+      content: "If a hybrid approach is what you require, we can help you maintain some services on-premises while leveraging the cloud for others, offering scalability without compromising on security"
+    },
+    {
+      title: "SAP Business Process Intelligence",
+      content: "Similar to the public version, we'll provide insights into your business processes to optimize workflows and inform data-driven decision-making"
+    },
+    {
+      title: "SAP S/4HANA (Private Cloud)",
+      content: "Transition to SAP S/4HANA, SAP's advanced ERP solution, within your private cloud or on-premises environment to access real-time data and streamlined processes."
+    }
+  ],
+  sections: [
+    {
+      type: "WHY_CHOOSE_US",
+      title: "Why Choose Marici for Rise with SAP Implementation Services",
+      content: "content needed",
+      points: [
+        {
+          title: "Expertise",
+          content: "Our team has extensive experience in SAP implementations and digital transformations across various industries"
+        },
+        {
+          title: "Customization",
+          content: "We tailor our services to align perfectly with your business objectives and growth plans"
+        },
+        {
+          title: "Scalability",
+          content: "Whether you choose Rise with SAP (Public) or (Private), we provide scalable solutions to accommodate your evolving business needs"
+        },
+        {
+          title: "Continuous Support",
+          content: " We offer ongoing support to ensure the smooth operation of your SAP environment and help you extract maximum value from your investment"
+        }
+      ]
+    }
+  ]
 };
 
 export default content;
--- src/constants/content/services/implementationConsultingService/erpServices/riseWithSapPublic.ts
+++ src/constants/content/services/implementationConsultingService/erpServices/riseWithSapPublic.ts
@@ -26,6 +26,31 @@ const content = {
       title: "SAP Business Network Services",
       content: "Enhance your collaboration with suppliers and customers through our services, facilitating smoother procurement, sales, and supply chain operations."
     }
+  ],
+  sections: [
+    {
+      type: "WHY_CHOOSE_US",
+      title: "Why Choose Marici for Rise with SAP Implementation Services",
+      content: "Embrace the future with confidence. Choose Marici for Rise with SAP Implementation Services, and together, we'll embark on a journey of digital transformation that elevates your company's services to new heights. Your digital journey starts here.",
+      points: [
+        {
+          title: "Expertise",
+          content: "Our team has extensive experience in SAP implementations and digital transformations across various industries"
+        },
+        {
+          title: "Customization",
+          content: "We tailor our services to align perfectly with your business objectives and growth plans"
+        },
+        {
+          title: "Scalability",
+          content: "Whether you choose Rise with SAP (Public) or (Private), we provide scalable solutions to accommodate your evolving business needs"
+        },
+        {
+          title: "Continuous Support",
+          content: " We offer ongoing support to ensure the smooth operation of your SAP environment and help you extract maximum value from your investment"
+        }
+      ]
+    }
   ]
 };
 
