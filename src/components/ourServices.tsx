79c125561f877b5c2a86aa7a95fd199cbc977c6a
resparx <ramprash.rspx@gmail.com>
2023-10-05 16:14:49 +0530

    fix: revamp our services

--- src/components/ourServices.tsx
+++ src/components/ourServices.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Bolt from "@/icons/bolt";
 import SectionPill from "./common/sectionPill";
 import Chart from "@/icons/chart";
@@ -5,6 +7,11 @@ import People from "@/icons/people";
 import Phone from "@/icons/phone";
 import Chip from "@/icons/chip";
 import Gear from "@/icons/gear";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Link from "next/link";
+import ArrowRight from "@/icons/arrow-right";
 
 const ServiceItem = ({
   title,
@@ -16,58 +23,73 @@ const ServiceItem = ({
   content: string;
 }) => {
   return (
-    <div className="flex flex-col gap-4 w-64">
-      <div className="flex justify-center">
-        <div className="rounded-2xl bg-sky-600 p-4">{icon}</div>
+    <div className="flex flex-col gap-4 w-[400px] rounded-xl bg-slate-800 shadow-xl p-8 mr-8 mt-8">
+      <div className="flex justify-start">
+        <div className="rounded-full bg-sky-600 p-4">{icon}</div>
       </div>
-      <p className="text-xl font-bold text-gray-900 text-center">{title}</p>
-      <p className="text-base text-gray-500 font-medium text-center">
+      <p className="text-2xl font-bold text-white">{title}</p>
+      <p className="text-base text-gray-300 font-normal overflow-hidden text-ellipsis line-clamp-2">
         {content}
       </p>
+      <Link className="text-gray-100 flex items-center gap-3 mt-2 text-base font-semibold" href="">
+        Learn more <ArrowRight className="fill-gray-100"/>
+      </Link>
     </div>
   );
 };
 
 const OurServices = () => {
+  const sliderSettings = {
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    variableWidth: true
+  };
   return (
-    <section className="p-24 px-48 flex flex-col gap-12 bg-white w-full">
-      <div className="flex flex-col items-center gap-4 mb-12">
-        <SectionPill title="Our Services" />
-        <p className="text-gray-900 text-4xl font-bold text-center">
-          Gain more insight into how people use your
-        </p>
-        <p className="text-gray-500 font-normal w-2/5 text-center">
+    <section className="p-24 px-48 flex flex-col gap-12 bg-white w-full mt-[120px]">
+      <div className="flex items-end gap-4">
+        <div className="flex flex-col w-[40%] gap-6">
+          <SectionPill title="Our Services" />
+          <p className="text-gray-900 text-5xl font-bold">
+            Gain more insight into how people use your
+          </p>
+        </div>
+        <p className="text-gray-500 text-xl font-medium w-2/5">
           With our integrated CRM, project management, collaboration and
           invoicing capabilities, you can manage every aspect of your business
           in one secure platform.
         </p>
       </div>
-      <div className="flex gap-36 justify-center flex-wrap">
+      <div className="w-full">
+      <Slider {...sliderSettings}>
         <ServiceItem
-          icon={<Bolt className="fill-white h-8" />}
+          icon={<Bolt className="fill-white h-6" />}
           title="Digital transformation and Consulting Services"
           content="Our consulting services empower your organization to navigate the digital landscape, optimize operations, and enhance customer experiences. Unlock your digital potential with Marici"
         />
         <ServiceItem
-          icon={<Chart className="fill-white h-8" />}
+          icon={<Chart className="fill-white h-6" />}
           title="Implementation and Rollout Services"
           content="Get a complete sales dashboard in the cloud. See activity, revenue and social metrics all in one place."
         />
         <ServiceItem
-        icon={<People className="fill-white h-8" />}
+          icon={<People className="fill-white h-6" />}
           title="Conversion and Migration Services"
           content="Our calendar lets you know what is happening with customer and projects so you"
         />
         <ServiceItem
-        icon={<Chip className="fill-white h-8" />}
+          icon={<Chip className="fill-white h-6" />}
           title="Application Management Services"
           content="A tool that lets you build a dream website even if you know nothing about web design or programming."
         />
         <ServiceItem
-        icon={<Phone className="fill-white h-8" />}
+          icon={<Phone className="fill-white h-6" />}
           title="Integration and Analytic Services"
           content="The first business platform to bring together all of your products from one place."
         />
+      </Slider>
       </div>
     </section>
   );
