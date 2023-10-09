2dd2bcc6925edbc8811b500f91969d206ff82941
resparx <ramprash.rspx@gmail.com>
2023-10-09 18:57:56 +0530

    fix: remove old image component

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -8,47 +8,6 @@ import classNames from "classnames";
 
 const ImageItem = ({ imgSrc, heading, content, theme }: any) => {
   return (
-    <div className="h-screen w-screen relative flex items-center justify-center">
-      <img className="absolute top-0 left-0" src={imgSrc} />
-      <div className="w-full h-full flex flex-col z-10 backdrop-blur-md items-center justify-center gap-3">
-        <h1
-          className={classNames("font-semibold", "text-7xl", {
-            "text-slate-800": theme === "DARK",
-            "text-white": theme !== "DARK",
-          })}
-        >
-          {heading}
-        </h1>
-        <p
-          className={classNames("font-medium", "text-xl", {
-            "text-slate-800": theme === "DARK",
-            "text-white": theme !== "DARK",
-          })}
-        >
-          {content}
-        </p>
-        <button
-          className={classNames(
-            "rounded-xl",
-            "px-4",
-            "py-3",
-            "bg-white",
-            "font-semibold",
-            {
-              "text-slate-800": theme === "DARK",
-              "text-white": theme !== "DARK",
-            }
-          )}
-        >
-          Know more
-        </button>
-      </div>
-    </div>
-  );
-};
-
-const ImageItem2 = ({ imgSrc, heading, content, theme }: any) => {
-  return (
     <div className="flex flex-col items-center w-full pt-40 ">
       <h1 className="text-6xl text-slate-900 font-semibold mb-4">{heading}</h1>
       <p className=" w-1/3 text-center text-lg text-slate-600 font-normal mb-4">
@@ -93,48 +52,44 @@ const ImageItem2 = ({ imgSrc, heading, content, theme }: any) => {
 };
 
 const Banner = () => {
+ 
   const settings = {
     infinite: true,
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
+    autoPlaySpeed: 1000
   };
 
   return (
     <section className="w-screen">
       <Slider {...settings}>
-        {/* <ImageItem
-          theme="DARK"
-          imgSrc="/static/images/banner1.jpg"
-          heading="SAP SuccesFactors"
-          content="Transform your human capital management experience to create a more flexible & engaged workforce"
-        /> */}
-        <ImageItem2
+        <ImageItem
           theme="DARK"
           imgSrc="/static/images/landing/banner_sap-hybris.png"
           heading="SAP Hybris"
           content="Understand your customers better and provide them with seamless & personalized products & services"
         />
-        <ImageItem2
+        <ImageItem
           theme="DARK"
           imgSrc="/static/images/landing/banner_digital-transformation.png"
           heading="Digital Transformation for Retail"
           content="Understand your customers better and provide them with seamless & personalized products & services"
         />
-        <ImageItem2
+        <ImageItem
           theme="DARK"
           imgSrc="/static/images/landing/banner_sap-s4.png"
           heading="S/4 HANA"
           content="Understand your customers better and provide them with seamless & personalized products & services"
         />
-        <ImageItem2
+        <ImageItem
           theme="DARK"
           imgSrc="/static/images/landing/banner_sap-successfactors.png"
           heading="SAP SuccessFactors"
           content="Understand your customers better and provide them with seamless & personalized products & services"
         />
-        <ImageItem2
+        <ImageItem
           theme="DARK"
           imgSrc="/static/images/landing/banner_sap-master-data-management.png"
           heading="Master Data Management"
