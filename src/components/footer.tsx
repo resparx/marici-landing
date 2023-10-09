c2159d5fb362a57da434620267a9690278b52eed
resparx <ramprash.rspx@gmail.com>
2023-10-10 01:56:14 +0530

    fix: footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,23 +9,23 @@ const Li = ({ children }: { children: any }) => {
 
 const Footer = () => {
   return (
-    <footer className="flex w-full bg-slate-900 px-32 py-24 justify-between">
-      <ul className="w-52">
+    <footer className="flex flex-wrap sm:flex-nowrap w-full bg-slate-900 px-4 sm:px-32 py-4 sm:py-24 justify-between">
+      <ul className="sm:w-52">
         <li className="mb-9">
           <img className="w-full" src="/static/images/maharshi_light.png" />
         </li>
         <Li>Email: info@maricisolutions.com</Li>
         <Li>Phone: +49 17641848023</Li>
       </ul>
-      <ul className="flex flex-col w-48 gap-3">
-        <li className="text-2xl font-semibold text-white mb-8">About us</li>
+      <ul className="flex flex-col w-40 sm:w-48 gap-1 sm:gap-3 mt-12">
+        <li className="text-2xl font-semibold text-white sm:mb-8 mb-2">About us</li>
         <Li>Who We Are</Li>
         <Li>Why MARICI</Li>
         <Li>Our Value Propositions</Li>
         <Li>How we deliver</Li>
       </ul>
-      <ul className="flex flex-col w-48 gap-3">
-        <li className="text-2xl font-semibold text-white mb-8">Solutions</li>
+      <ul className="flex flex-col w-40 sm:w-48 gap-1 sm:gap-3 mt-12">
+        <li className="text-2xl font-semibold text-white sm:mb-8 mb-2">Solutions</li>
         <Li>Solutions</Li>
         <Li>S/4 HANA</Li>
         <Li>Digital Transformation for Retail</Li>
@@ -34,24 +34,24 @@ const Footer = () => {
         <Li>Master Data Management</Li>
         <Li>RPA</Li>
       </ul>
-      <ul className="flex flex-col w-48 gap-3">
-        <li className="text-2xl font-semibold text-white mb-8">Services</li>
+      <ul className="flex flex-col w-40 sm:w-48 gap-1 sm:gap-3 mt-12">
+        <li className="text-2xl font-semibold text-white sm:mb-8 mb-2">Services</li>
         <Li>Consulting</Li>
         <Li>Implementations</Li>
         <Li>AppLication Development</Li>
         <Li>Business IntelLigence & Performance Optimization</Li>
         <Li>Training & Support</Li>
       </ul>
-      <ul className="flex flex-col w-48 gap-3">
-        <li className="text-2xl font-semibold text-white mb-8">Address</li>
+      <ul className="flex flex-col w-40 sm:w-48 gap-1 sm:gap-3 mt-12">
+        <li className="text-2xl font-semibold text-white sm:mb-8 mb-2">Address</li>
         <Li>HQ – Germany: Maxfeldstraße 52, 90409 Nürnberg</Li>
         <Li>USA Subsidiary: Maharshi Solutions Inc</Li>
         <Li>1912 Eastchester Dr Suite 108, High Point, North CaroLina</Li>
       </ul>
-      <ul className="flex flex-col w-48 gap-3">
-        <li className="text-2xl font-semibold text-white mb-8">Follow Us</li>
+      <ul className="flex flex-col w-full sm:w-48 gap-1 sm:gap-3 mt-12">
+        <li className="text-2xl font-semibold text-white sm:mb-8 mb-2 text-center sm:text-start">Follow Us</li>
         <li>
-          <div className="flex gap-2">
+          <div className="flex justify-around sm:justify-start gap-2">
             <Twitter className="h-8 fill-slate-200" />
             <Linkedin className="h-8 fill-slate-200" />
             <Facebook className="h-8 fill-slate-200" />
