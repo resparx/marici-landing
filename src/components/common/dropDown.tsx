1c8a32dab3edf8f1cc4e00f518c7efeff5de8c14
resparx <ramprash.rspx@gmail.com>
2023-10-10 10:04:47 +0530

    feat: add deps

---
+++ src/components/common/dropDown.tsx
@@ -0,0 +1,76 @@
+import {useRef, useState} from "react";
+import Popper from "popper.js";
+
+const Dropdown = ({ color, children }) => {
+  const [dropdownPopoverShow, setDropdownPopoverShow] = useState(false);
+  const btnDropdownRef = useRef();
+  const popoverDropdownRef = useRef();
+  const openDropdownPopover = () => {
+    new Popper(btnDropdownRef?.current, popoverDropdownRef.current, {
+      placement: "bottom-start"
+    });
+    setDropdownPopoverShow(true);
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  // bg colors
+  let bgColor;
+  color === "white"
+    ? (bgColor = "bg-gray-800")
+    : (bgColor = "bg-" + color + "-500");
+  return (
+    <>
+      <div className="flex flex-wrap">
+        <div className="w-full sm:w-6/12 md:w-4/12 px-4">
+          <div className="relative inline-flex align-middle w-full">
+            <div
+              style={{ transition: "all .15s ease" }}
+              type="button"
+              ref={btnDropdownRef}
+              onClick={() => {
+                dropdownPopoverShow
+                  ? closeDropdownPopover()
+                  : openDropdownPopover();
+              }}
+            >
+              {children}
+            </div>
+            <div
+              ref={popoverDropdownRef}
+              className={
+                (dropdownPopoverShow ? "block " : "hidden ") +
+                (color === "white" ? "bg-white " : bgColor + " ") +
+                "text-base z-50 float-left py-2 list-none text-left rounded shadow-lg mt-1"
+              }
+              style={{ minWidth: "12rem" }}
+            >
+              <a
+                href="#pablo"
+                className={
+                  "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent " +
+                  (color === "white" ? " text-gray-800" : "text-white")
+                }
+                onClick={e => e.preventDefault()}
+              >
+                Action
+              </a>
+              <a
+                href="#pablo"
+                className={
+                  "text-sm py-2 px-4 font-normal block w-full whitespace-no-wrap bg-transparent " +
+                  (color === "white" ? " text-gray-800" : "text-white")
+                }
+                onClick={e => e.preventDefault()}
+              >
+                Seprated link
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Dropdown
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -22,71 +22,69 @@ const SubNav = ({ heading, items }: any) => {
 
 const Navbar = () => {
   return (
-    <div className="w-full h-20 flex justify-between px-52 fixed bg-white items-center z-10">
-      <div>
-        <img className="w-44" src="/static/images/mahrashi_dark.png" />
-      </div>
+    <div className="w-full h-20 flex justify-between px-4 sm:px-52 fixed bg-white items-center z-10">
+      <Link href="/">
+        <img
+          className="w-[40%] sm:w-44"
+          src="/static/images/mahrashi_dark.png"
+        />
+      </Link>
+      
       <a>
-      <ul className="flex gap-12 align-middle h-20">
-        <li>
-          <Link className="text-slate-800 h-full" href="/">
-            <p className="peer font-medium h-full flex items-center">
-              Services
-              <Down className="inline ml-1" />
-            </p>
-            <ul className="hidden hover:flex peer-hover:flex flex-col gap-6 absolute top-20 bg-white backdrop-blur-sm bg-opacity-80 p-3 rounded-b-lg rounded-br-lg">
-              <li>
-                <Link href="/services/implementation-and-consulting-service">
-                  <p>Implementation and Consulting Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services/upgrade-migration-services">
-                  <p>Upgrade and Migration Services (SAP/CRM) </p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services/support-maintenance-services">
-                  <p>ERP/Cx Support and Maintenance Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services/application-development-frontend-services">
-                  <p>Application Developement and Front end Services</p>
-                </Link>
-              </li>
-              <li>
-                <Link href="/services/staffing-services">
-                  <p>Staffing Services</p>
-                </Link>
-              </li>
-            </ul>
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <Link className="text-slate-800" href="/case-studies">
-            <p className="font-medium">
-              Case studies
-            </p>
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <Link className="text-slate-800" href="/about-us">
-            <p className="font-medium">
-              About us
-            </p>
-          </Link>
-        </li>
-        <li className="flex items-center">
-          <Link className="text-slate-800" href="/contact-us">
-            <p className="font-medium">
-              Contact us
-            </p>
-          </Link>
-        </li>
-      </ul>
+        <ul className="hidden sm:flex gap-12 align-middle h-20">
+          <li>
+            <Link className="text-slate-800 h-full" href="/">
+              <p className="peer font-medium h-full flex items-center">
+                Services
+                <Down className="inline ml-1" />
+              </p>
+              <ul className="hidden hover:flex peer-hover:flex flex-col gap-6 absolute top-20 bg-white backdrop-blur-sm bg-opacity-80 p-3 rounded-b-lg rounded-br-lg">
+                <li>
+                  <Link href="/services/implementation-and-consulting-service">
+                    <p>Implementation and Consulting Services</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/upgrade-migration-services">
+                    <p>Upgrade and Migration Services (SAP/CRM) </p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/support-maintenance-services">
+                    <p>ERP/Cx Support and Maintenance Services</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/application-development-frontend-services">
+                    <p>Application Developement and Front end Services</p>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/services/staffing-services">
+                    <p>Staffing Services</p>
+                  </Link>
+                </li>
+              </ul>
+            </Link>
+          </li>
+          <li className="flex items-center">
+            <Link className="text-slate-800" href="/case-studies">
+              <p className="font-medium">Case studies</p>
+            </Link>
+          </li>
+          <li className="flex items-center">
+            <Link className="text-slate-800" href="/about-us">
+              <p className="font-medium">About us</p>
+            </Link>
+          </li>
+          <li className="flex items-center">
+            <Link className="text-slate-800" href="/contact-us">
+              <p className="font-medium">Contact us</p>
+            </Link>
+          </li>
+        </ul>
       </a>
-      <div className="flex items-center">
+      <div className="hidden sm:flex items-center">
         <Linkedin fill="#146ebe" height={32} />
         <img className="h-10" src="/static/images/sap-silver.png" />
       </div>
